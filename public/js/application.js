7191d8f3e80c744796a039a645dd105d9bb3f359
Kevin Kwon <kvnkwon@gmail.com>
2014-12-03 19:15:49 -0500

    added mousetrap

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
 
-  $(".landing-bg").backstretch("https://ununsplash.imgix.net/46/unsplash_52c319226cefb_1.JPG?q=75&fm=jpg&s=72d1a0318ab967123bb38fc959042808");
+  $(".landing-bg").backstretch("/img/home.jpeg");
 
   $(".texture-form").backstretch("/img/footer_lodyas.png");
 
+  Mousetrap.bind('enter', function () {
+    $(".crowd-cheer").html("MAKE SOME NOISE!!!!!")
+  }, 'keyup')
 });
